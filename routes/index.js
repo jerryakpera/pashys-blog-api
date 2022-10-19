49131dd75389b3497d3f54aaa58d2d356c74c42b
Jeremiah Akpera <jerryakpera@gmail.com>
2022-10-19 07:50:31 +0530

    Added test route

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,8 @@ const commentsRoute = require('./comments');
 const router = express.Router();
 
 router.use('/comments', commentsRoute);
+router.get('/test', (req, res) => {
+  return res.send('WORKING');
+});
 
 module.exports = router;
